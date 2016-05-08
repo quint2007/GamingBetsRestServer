1776b4382f888a75f8fa433d6f107e32967e32a9
quint2007 <quint0095@gmail.com>
2016-05-08 18:56:48 +0200

    No hardcoded DB Password

--- gamingBetRestServer/src/main/java/com/gabmingbets/gamingbetrestserver/domain/service/TicketFacadeREST.java
+++ gamingBetRestServer/src/main/java/com/gabmingbets/gamingbetrestserver/domain/service/TicketFacadeREST.java
@@ -37,14 +37,14 @@ public class TicketFacadeREST extends AbstractFacade<Ticket> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void create(Ticket entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, Ticket entity) {
         super.edit(entity);
     }
@@ -57,21 +57,21 @@ public class TicketFacadeREST extends AbstractFacade<Ticket> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public Ticket find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<Ticket> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<Ticket> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
--- gamingBetRestServer/src/main/java/com/gabmingbets/gamingbetrestserver/domain/service/UserFacadeREST.java
+++ gamingBetRestServer/src/main/java/com/gabmingbets/gamingbetrestserver/domain/service/UserFacadeREST.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -25,7 +26,7 @@ import javax.ws.rs.core.MediaType;
  * @author Andre
  */
 @Stateless
-@Path("com.gabmingbets.gamingbetrestserver.domain.user")
+@Path("users")
 public class UserFacadeREST extends AbstractFacade<User> {
 
     @PersistenceContext(unitName = "com.gabmingbets_gamingBetRestServer_war_1.0PU")
@@ -37,14 +38,14 @@ public class UserFacadeREST extends AbstractFacade<User> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void create(User entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, User entity) {
         super.edit(entity);
     }
@@ -57,21 +58,30 @@ public class UserFacadeREST extends AbstractFacade<User> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public User find(@PathParam("id") Integer id) {
         return super.find(id);
     }
+    
+    @GET
+    @Path("/name/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User findByName(@PathParam("name") String name){
+        TypedQuery<User> query = getEntityManager().createNamedQuery("User.findByUserName", User.class).setParameter("userName", name);
+        return query.getSingleResult();
+    }
+    
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<User> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<User> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
